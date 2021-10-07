b2dbb384a47c75b97354a16e4dfe7a0785327bde
dldydrhkd <48857296+dldydrhkd@users.noreply.github.com>
2021-10-07 19:50:54 +0900

    소셜 로그인 만들기

---
+++ login.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport'); //passport 추가
+var NaverStrategy = require('passport-naver').Strategy;
+
+router.get('/naver/login', passport.authenticate('naver',null),function(req,res){
+    console.log("/naver/login")
+  });
+  
+router.get('/naver/login/callback', passport.authenticate('naver', {
+    successRedirect: '/',
+    failureRedirect: '/naver/login'
+  }));
+  
+passport.use(new NaverStrategy({
+    clientID: 'sQnAiTwCTKvgHeKe1M91',
+    clientSecret: 'lrXYXxnuww',
+    callbackURL: '/naver/login/callback'
+  },
+function(accessToken, refreshToken, profile, done){
+    process.nextTick(function(){
+    })
+  }))
+  
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+passport.deserializeUser(function(req, user, done) {
+  
+  // passport로 로그인 처리 후 해당 정보를 session에 담는다.
+  req.session.sid = user.name;
+  console.log("Session Check :" +req.session.sid);
+    done(null, user);
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');   //express module을 불러온다
 const app = express();              //app에 express() 함수의 변환 값을 저장
+var passport = require('passport');
+var NaverStrategy = require('passport-naver').Strategy;
+var router = express.Router()
+var loginRouter = require('./login.js');
+
+client_id = 'sQnAiTwCTKvgHeKe1M91'
+cline_secret = 'lrXYXxnuww'
+redirectURI = 'localhost:8080'
 
 app.get('/',function(req,res){
   res.sendFile(__dirname + '/index.html');
@@ -9,8 +17,10 @@ app.get('/search', function(req,res){
   console.log(req.query.search);
 })
 
-app.get('/login', function(req,res){
-  res.sendFile(__dirname + '/login.html')
+app.get('/naver/login', loginRouter);
+
+app.get('/naver/login/callback', function(req,res){
+  res.sendFile(__dirname + '/index.html');
 })
 
 app.listen(8080, function(){
